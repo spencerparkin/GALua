e463e3414afec7f494c0b51035ec7b6e922f0950
Spencer Parkin <spencer.parkin@gmail.com>
2013-02-02 11:14:36 -0700

    test script

--- GALua.lua
+++ GALua.lua
@@ -1,4 +1,4 @@
--- Test the GALua module.
+-- GALua.lua -- Test the GALua module.
 
 --package.cpath = package.cpath .. ";.\\Debug\\?.dll"
 package.cpath = package.cpath .. ";C:\\GALua\\Build\\Debug\\?.dll"
@@ -24,10 +24,72 @@ local basisVecIPFunc = function( i, j )
 end
 galua.def_sig( basisVecIPFunc )
 
--- We're now ready to do some GA!
-local multiVecA = galua( "3*e1 + 2*e1^e2 - 4" )
-local multiVecB = galua( "-2 + e3 + e3^e1" )
-local multiVecC = multiVecA * multiVecB
-print( "result = " .. tostring( multiVecC ) )
-local mag = #multiVecC
+-- Print out an array of multi-vectors.
+function PrintMultiVecArray( mvArray )
+	for idx, multiVec in ipairs( mvArray ) do
+		local result = tostring( multiVec )
+		print( idx .. " -> " .. result )
+	end
+end
+
+-- Test with these multi-vectors.  We will do a series of tests
+-- where in each test we generate an array of multi-vectors.
+-- Each test should generate the same array of multi-vectors.
+local mvStringA = "1 + e2^e2"
+local mvStringB = "e1 - 3*e1^e2^e3"
+
+--===========================================================
+-- Test API functions.
+--===========================================================
+print( "Testing API functions..." )
+local mvArray = {}
+mvArray[ #mvArray + 1 ] = galua.from_string( mvStringA )
+mvArray[ #mvArray + 1 ] = galua.from_string( mvStringB )
+mvArray[ #mvArray + 1 ] = galua.add( mvArray[1], mvArray[2] )
+mvArray[ #mvArray + 1 ] = galua.sub( mvArray[1], mvArray[2] )
+mvArray[ #mvArray + 1 ] = galua.gp( mvArray[1], mvArray[2] )
+mvArray[ #mvArray + 1 ] = galua.ip( mvArray[1], mvArray[2] )
+mvArray[ #mvArray + 1 ] = galua.op( mvArray[1], mvArray[2] )
+mvArray[ #mvArray + 1 ] = galua.negate( mvArray[1] )
+mvArray[ #mvArray + 1 ] = galua.reverse( mvArray[2] )
+local mag = galua.mag( mvArray[1] )
+PrintMultiVecArray( mvArray )
+print( "mag = " .. galua.to_string( mag ) )
+
+--===========================================================
+-- Test operator overload functions.
+--===========================================================
+print( "Testing operator overload functions..." )
+mvArray = {}
+mvArray[ #mvArray + 1 ] = galua( mvStringA )
+mvArray[ #mvArray + 1 ] = galua( mvStringB )
+mvArray[ #mvArray + 1 ] = mvArray[1] + mvArray[2]
+mvArray[ #mvArray + 1 ] = mvArray[1] - mvArray[2]
+mvArray[ #mvArray + 1 ] = mvArray[1] * mvArray[2]
+mvArray[ #mvArray + 1 ] = mvArray[1] % mvArray[2]
+mvArray[ #mvArray + 1 ] = mvArray[1] ^ mvArray[2]
+mvArray[ #mvArray + 1 ] = -mvArray[1]
+mvArray[ #mvArray + 1 ] = galua.reverse( mvArray[2] )	-- No overload for this.
+local mag = #mvArray[1]
+PrintMultiVecArray( mvArray )
 print( "mag = " .. tostring( mag ) )
+
+--===========================================================
+-- Test coercion.
+--===========================================================
+print( "Testing coercion..." )
+mvArray = {}
+mvArray[ #mvArray + 1 ] = galua( mvStringA )
+mvArray[ #mvArray + 1 ] = galua( mvStringB )
+mvArray[ #mvArray + 1 ] = mvStringA + mvArray[2]
+mvArray[ #mvArray + 1 ] = mvArray[1] - mvStringB
+mvArray[ #mvArray + 1 ] = galua.gp( mvStringA, mvStringB )
+mvArray[ #mvArray + 1 ] = mvStringA % mvArray[2]
+mvArray[ #mvArray + 1 ] = mvArray[1] ^ mvStringB
+mvArray[ #mvArray + 1 ] = galua.negate( mvStringA )
+mvArray[ #mvArray + 1 ] = galua.reverse( mvStringB )
+local mag = galua.mag( mvStringA )
+PrintMultiVecArray( mvArray )
+print( "mag = " .. galua.to_string( mag ) )
+
+-- GALua.lua
