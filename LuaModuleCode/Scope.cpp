9f530882d5af70d03641f482e9d26c6da5e3e880
Spencer Parkin <spencer.parkin@gmail.com>
2013-02-09 16:11:57 -0700

    comments on scope

--- LuaModuleCode/Scope.cpp
+++ LuaModuleCode/Scope.cpp
@@ -12,6 +12,13 @@
 #include "Scope.h"
 
 //=========================================================================================
+// Here we are trying to synthesize Lua's scoping algorithm.
+// This is a dubious thing to do, because we may not get it right,
+// and even if we do get it right, if Lua's scoping algorithm ever
+// changes, this code becomes invalid.  The use of this function
+// is therefore limited to only one special case that I'm handling,
+// because I want to.
+//
 // Push onto the stack the value of the given variable at the current scope.
 // Push nil if it does not exist.
 int l_scope_lookup( lua_State* L, const char* varName )
