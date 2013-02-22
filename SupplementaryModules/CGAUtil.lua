e16eedc69ed3ec1b028990e970fb146f5ba5d17c
Spencer Parkin <spencer.parkin@gmail.com>
2013-02-21 19:35:43 -0700

    more work on cga util companion module

--- SupplementaryModules/CGAUtil.lua
+++ SupplementaryModules/CGAUtil.lua
@@ -41,7 +41,7 @@ local CGAGeometry = {}
 
 ------------------------------------------------------------------------
 function CGAGeometry:Setup()
-	self.epsilon = 0.001
+	self.epsilon = 0.0001
 end
 
 ------------------------------------------------------------------------
@@ -51,6 +51,43 @@ function CGAGeometry:New( geo )
 	self.__index = self
 	geo:Setup()
 	return geo
+end
+
+------------------------------------------------------------------------
+-- Notice that we do not attempt to check whether the given multi-vector
+-- value is homogeneous of a given grade.  We assume that it is.
+-- Furthermore, we assume that it is such an element that may be factored
+-- as a blade.  If these assumptions are not met, the result of this
+-- function is simply undefined.
+function CGAGeometry:GrabDualGeometry( blade, dualGrade )
+	
+	-- Ultimately we want to return a dual geometry.
+	local dualGeo
+	
+	-- We're assuming that the given blade dually represents the geometry.
+	local dual = true
+	
+	-- Calculate the grade of the blade directly repreentative of the geometry.
+	local dirGrade = 5 - dualGrade
+	
+	-- Is the blade of the direct grade?
+	local dirGeo = blade[ dirGrade ]
+	if dirGeo ~= 0 then
+	
+		-- Yes.  We were given a blade directly representative of the geometry.
+		dualGeo = dirGeo * -I
+		dual = false
+		
+	else
+	
+		-- No.  It then must be of the dual grade.
+		dualGeo = blade[1]
+		
+	end
+	
+	-- Return the geometry in dual form and whether or not we were given the blade in dual form.
+	return dualGeo, dual
+	
 end
 
 ------------------------------------------------------------------------
@@ -72,9 +109,10 @@ end
 
 ------------------------------------------------------------------------
 function CGAPoint:Setup()
+	self.type = "point"
 	self.dual = self.dual or true
 	self.weight = self.weight or 1
-	self.center = self.center or galua( "0" )
+	self.center = self.center or 0
 end
 
 ------------------------------------------------------------------------
@@ -102,17 +140,8 @@ end
 function CGAPoint:DecomposeBlade( blade )
 
 	-- Try to find the dual point that is the point represented by the given blade.
-	local dual = true
-	local dualPoint
-	local dirPoint = blade[4]
-	if dirPoint ~= 0 then
-		dualPoint = dirPoint * -I
-		dual = false
-	else
-		dualPoint = blade[1]
-	end
-	
 	-- If we didn't find it, then we were not given a point.
+	local dualPoint, dual = self:GrabDualGeometry( blade, 1 )
 	if dualPoint == 0 then
 		return false
 	end
@@ -122,21 +151,179 @@ function CGAPoint:DecomposeBlade( blade )
 	if ( #weight ):tonumber() < self.epsilon then
 		return false	-- In this case what we have is really a dual plane.
 	end
+	dualPoint = dualPoint / weight
+	local center = no_ni .. ( dualPoint ^ no_ni )
+	if ( #( -2 * dualPoint .. no - center * center ) ):tonumber() >= self.epsilon then
+		return false	-- In thsi case what we have is really a dual sphere.
+	end
+	
+	-- Store the decomposition.
 	self.dual = dual
 	self.weight = weight
-	dualPoint = dualPoint / weight
-	self.center = no_ni .. ( dualPoint ^ no_ni )
+	self.center = center
 	
 	-- Return success.
 	return true
 	
 end
 
--- TODO: Implement the rest of the CGA geometries.
+------------------------------------------------------------------------
+function CGAFlatPoint:Setup()
+	self.type = "flatpoint"
+end
+
+------------------------------------------------------------------------
+function CGAFlatPoint:ComposeBlade()
+end
+
+------------------------------------------------------------------------
+function CGAFlatPoint:DecomposeBlade( blade )
+end
+
+------------------------------------------------------------------------
+function CGAPointPair:Setup()
+	self.type = "pointpair"
+end
+
+------------------------------------------------------------------------
+function CGAPointPair:ComposeBlade()
+end
+
+------------------------------------------------------------------------
+function CGAPointPair:DecomposeBlade( blade )
+end
+
+------------------------------------------------------------------------
+function CGALine:Setup()
+	self.type = "line"
+	self.dual = self.dual or true
+	self.weight = self.weight or 1
+	self.center = self.center or 0
+	self.normal = self.normal or e2
+end
+
+------------------------------------------------------------------------
+function CGALine:ComposeBlade()
+
+	-- Formulate a dual or direct line.
+	if self.dual then
+		blade = self.weight * ( self.normal + self.center ^ self.normal ^ ni ) * Ie
+	else
+		blade = self.weight * ( self.normal + self.center ^ self.normal ^ ni ) * -no_ni
+	end
+	
+	-- Return the blade representative of the line.
+	return blade
+	
+end
+
+------------------------------------------------------------------------
+function CGALine:DecomposeBlade( blade )
+
+	-- Try to find the dual line that is the line represented by the given blade.
+	-- If we didn't find it, then we were not give a line.
+	local dual = true
+	local dualLine, dual = self:GrabDualGeometry( blade, 2 )
+	
+	-- Decompose the line.
+	local normal = ( no .. ( ( dualLine * -Ie ) ^ ni ) )[1]		-- Take vector part to kill round-off error.
+	local weight = #normal
+	if weight:tonumber() < self.epsilon then
+		return false		-- In this case, I believe that we may have been given is a free blade.
+	end
+	normal = normal / weight
+	dualLine = dualLine / weight
+	local center = ( -( no .. L ) * normal )[1]		-- This will be the point on the line closest to the origin.
+	
+	-- TODO: How else do we need to return false if we're not actually a dual line?
+	
+	-- Store the decomposition.
+	self.dual = dual
+	self.weight = weight
+	self.normal = normal
+	self.center = center
+	
+	-- Return success.
+	return true
+	
+end
+
+------------------------------------------------------------------------
+function CGACircle:Setup()
+	self.type = "circle"
+end
+
+------------------------------------------------------------------------
+function CGACircle:ComposeBlade()
+end
+
+------------------------------------------------------------------------
+function CGACircle:DecomposeBlade( blade )
+end
+
+------------------------------------------------------------------------
+function CGAPlane:Setup()
+	self.type = "plane"
+	self.dual = self.dual or true
+	self.weight = self.weight or 1
+	self.center = self.center or 0
+	self.normal = self.normal or e2
+end
+
+------------------------------------------------------------------------
+function CGAPlane:ComposeBlade()
+
+	-- Formulate a dual plane.
+	local blade = self.weight * ( self.normal + ( self.normal .. self.center ) * ni )
+	
+	-- Make it a direct plane, if needed.
+	if not self.dual then
+		blade = blade * I
+	end
+	
+	-- Return a vector representative of the plane.
+	return blade
+	
+end
+
+------------------------------------------------------------------------
+function CGAPlane:DecomposeBlade( blade )
+
+	-- Try to find the dual plane that is the plane represented by the given blade.
+	-- If we didn't find it, then we were not give a plane.
+	local dualPlane, dual = self:GrabDualGeometry( blade, 1 )
+	if dualPlane == 0 then
+		return false
+	end
+	
+	-- Decompose the plane.
+	if ( dualPlane .. ni ) ~= 0 then
+		return false		-- In this case, we were given a point or sphere.
+	end
+	local normal = no .. ( dualPlane ^ ni )
+	local weight = #normal
+	if weight:tonumber() < self.epsilon then
+		return false		-- In this case, we were given the null-point at infinity.
+	end
+	normal = normal / weight
+	dualPlane = dualPlane / weight
+	local center = ( -( dualPlane .. no ) * normal )[1]		-- This will be the point on the plane closest to the origin.
+	
+	-- Store the decomposition.
+	self.dual = dual
+	self.weight = weight
+	self.normal = normal
+	self.center = center
+	
+	-- Return success.
+	return true
+
+end
 
 ------------------------------------------------------------------------
 -- The default sphere is a degenerate sphere, which is a point.
 function CGASphere:Setup()
+	self.type = "sphere"
 	self.dual = self.dual or true
 	self.weight = self.weight or 1
 	self.center = self.center or galua( "0" )
@@ -162,20 +349,14 @@ function CGASphere:ComposeBlade()
 end
 
 ------------------------------------------------------------------------
+-- Note that if we were really given a point, we simply return a
+-- degenerate sphere.  So any check for geometric type should test
+-- points before spheres.
 function CGASphere:DecomposeBlade( blade )
 
 	-- Try to find the dual sphere that is the sphere represented by the given blade.
-	local dual = true
-	local dualSphere
-	local dirSphere = blade[4]
-	if dirSphere ~= 0 then
-		dualSphere = dirSphere * -I
-		dual = false
-	else
-		dualSphere = blade[1]
-	end
-	
 	-- If we didn't find it, then we were not given a sphere.
+	local dualSphere, dual = self:GrabDualGeometry( blade, 1 )
 	if dualSphere == 0 then
 		return false
 	end
