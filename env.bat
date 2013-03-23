4d3bc77cc85516f4371edf65f5cf6f3f9abee603
Spencer T. Parkin <spencer.parkin@gmail.com>
2013-03-22 22:44:34 -0600

    up and running with jamplus!  :)

--- env.bat
+++ env.bat
@@ -5,8 +5,8 @@ rem an earlier version of the interpreter on the system.  Our module is not comp
 set PATH=C:/GALua/lua-5.2.1/bin;D:/GALua/lua-5.2.1/bin;%PATH%
 
 rem Set the LUA_CPATH env-var so that Lua can find our Lua modules written in C++.
-set GALUA_CPATH=C:/GALua/Build/GALua/Debug/?.dll;D:/GALua/Build/GALua/Debug/?.dll
-set GALUA_CPATH=%GALUA_CPATH%;C:/GALua/Build/GALua/Release/?.dll;D:/GALua/Build/GALua/Release/?.dll
+set GALUA_CPATH=C:/GALua/Build/?.debug.dll;D:/GALua/Build/?.debug.dll
+set GALUA_CPATH=%GALUA_CPATH%;C:/GALua/Build/?.release.dll;D:/GALua/Build/?.release.dll
 if "%LUA_CPATH%"=="" (
 	set LUA_CPATH=%GALUA_CPATH%;;
 ) else (
